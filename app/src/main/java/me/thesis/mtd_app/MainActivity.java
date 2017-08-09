c39f9d39d1d9c6b5d0d3c8fefe3cee5ec8d9aa70
Maxine Hilvano <jem_1228@rocketmail.com>
2017-08-09 13:19:33 +0800

    minor changes to listview

--- app/src/main/java/me/thesis/mtd_app/MainActivity.java
+++ app/src/main/java/me/thesis/mtd_app/MainActivity.java
@@ -1,7 +1,10 @@
 package me.thesis.mtd_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final ImageButton recent=(ImageButton)findViewById(R.id.main_recent);
+        recent.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,Recent.class);
+                startActivity(i);
+            }
+        });
+
+        final ImageButton fave=(ImageButton)findViewById(R.id.main_fave);
+        fave.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,Favorite.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/me/thesis/mtd_app/Recent.java
@@ -0,0 +1,12 @@
+package me.thesis.mtd_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Recent extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recents);
+    }
+}
