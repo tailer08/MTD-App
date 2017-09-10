d2382fbcd1d74501cd5843595346e1e5ccb17c90
Khalile <kkpujante@gmail.com>
2017-09-10 15:43:24 +0800

    Text To Speech Functionality

--- app/src/main/java/me/thesis/mtd_app/WordView.java
+++ app/src/main/java/me/thesis/mtd_app/WordView.java
@@ -3,21 +3,28 @@ package me.thesis.mtd_app;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 import me.thesis.mtd_app.db.DBHandler;
 
-public class WordView extends AppCompatActivity {
+public class WordView extends AppCompatActivity implements View.OnClickListener, TextToSpeech.OnInitListener {
 
     TextView word;
     TextView defn;
+    TextToSpeech tts;
+    Button buttonSpeak;
+    String currentWord;
 
     private void setText(String str) {
         String wrd = "";
@@ -73,6 +80,7 @@ public class WordView extends AppCompatActivity {
         }
         word.setText(wrd);
         defn.setText(dfn);
+        currentWord = wrd;
     }
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +89,10 @@ public class WordView extends AppCompatActivity {
         setContentView(R.layout.activity_dictionary);
 
         final ImageButton close=(ImageButton)findViewById(R.id.word_close);
+        buttonSpeak = (Button) findViewById(R.id.button_speak);
+        buttonSpeak.setOnClickListener(this);
+        tts = new TextToSpeech(this, this);
+
         close.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -133,5 +145,27 @@ public class WordView extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_speak:
+                tts.speak(currentWord, TextToSpeech.QUEUE_FLUSH, null);
+                break;
+        }
+    }
+
+    @Override
+    public void onInit(int status) {
+        if( status == TextToSpeech.SUCCESS){
+            Locale US = tts.getLanguage();
+            int result = tts.setLanguage(US);
+            if(result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
+                Log.d("mtd", "Language not supported");
+            }else{
+
+            }
+        }
     }
 }
